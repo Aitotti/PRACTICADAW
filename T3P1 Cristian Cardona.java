41c63f691dae8a778ee40f72eb234e355461271d
CrisU <135056661+Onledw@users.noreply.github.com>
2023-11-24 13:45:16 +0100

    Add files via upload

---
+++ T3P1 Cristian Cardona.java
@@ -0,0 +1,59 @@
+package bucleforI;
+
+import java.util.Scanner;
+
+public class T3P1 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int op=0;
+	do {
+		System.out.println("1:CONTADOR DE PALABRAS \n2:SALIR DEL PROGRAMA");
+		op=sc.nextInt();
+		if(op==1) {
+				int numero=0;
+				System.out.println("INTRODUZCA LA FRASE");
+				String mensaje = sc.nextLine();//Si es un poco cutre pero no he encontrado otra forma
+				mensaje = sc.nextLine();// de que no se salte la linea de codigo y no de errores;
+				mensaje = mensaje.trim();
+				mensaje = mensaje.replaceAll("\\s+", " ");
+				String[] palabras = mensaje.split(" ");
+				numero = palabras.length;
+				System.out.println("NUMERO DE PALABRAS: " + numero);
+				break;
+		} else if (op == 2) {
+            System.out.println("SALIENDO DEL PROGRAMA");
+            sc.close();
+            return;
+        } else {
+            System.out.println("SI, SI MUY LISTO\nSI ES QUE DE VERDAD\n ⠀⠀⠀⠀⠀⠀⠀⠀" + 
+            			"	⢀⡴⠊⠉⠉⢉⠏⠻⣍⠑⢲⠢⠤⣄⣀⠀⠀⠀⠀⠀⠀⠀\r\n"
+            		+ "⠀⠀⠀⠀⠀⠀⠀⠀⣻⣿⢟⣽⠿⠯⠛⡸⢹⠀⢹⠒⣊⡡⠜⠓⠢⣄⠀⠀⠀⠀\r\n"
+            		+ "⠀⠀⠀⠀⠀⠀⢀⡜⣿⣷⣽⠓⠀⢠⢂⣣⠋⠂⣾⠼⢌⠳⢄⢀⡠⠜⣣⡀⠀⠀\r\n"
+            		+ "⠀⠀⠀⠀⠀⢠⢻⢱⣭⠷⠤⢅⠴⣡⡻⠃⠀⢠⠁⠀⢀⡱⠜⠍⢔⠊⠀⠹⡄⠀\r\n"
+            		+ "⠀⠀⠀⠀⢀⣷⠌⠚⠷⠆⠠⠶⠭⢒⣁⠀⣤⠃⣀⢔⢋⡤⠊⠑⣄⠳⣄⠀⣧⠀\r\n"
+            		+ "⠀⠀⠀⠀⠀⠑⠦⣀⡤⣄⠄⢄⣀⣠⣒⢦⡄⠩⠷⠦⠊⠀⠀⠀⠈⠣⡏⠢⣿⠀\r\n"
+            		+ "⠀⠀⠀⠀⠀⠀⣸⢫⠟⣝⠞⣼⢲⡞⣞⠋⠋⠉⠋⠓⡄⠀⠀⠀⠀⠀⣨⠂⢸⡅\r\n"
+            		+ "⠀⠀⠀⠀⠀⣰⠃⡨⠊⢀⡠⡌⢘⢇⠞⠀⠀⠀⠀⠂⠡⡄⠀⠀⢀⠞⢁⠔⢹⡇\r\n"
+            		+ "⠀⠀⠀⠀⣰⣣⠞⢀⠔⢡⢢⠇⡘⠌⠀⠀⠀⠀⠀⠀⠠⡌⠢⡔⢁⡴⠁⠀⢸⠃\r\n"
+            		+ "⠀⠀⠀⢠⠟⠁⠠⢊⠔⣡⢸⠀⠃⠁⠀⠀⠀⠀⠀⠀⠀⣯⠂⡀⢪⡀⠀⠀⢸⠀\r\n"
+            		+ "⠀⢀⠔⣁⠐⠨⠀⠀⠈⠀⢄⠘⡀⠀⠈⢆⠀⠀⠀⠀⡠⢁⠜⠙⢦⠙⣦⠀⢸⠀\r\n"
+            		+ "⡴⠁⠘⡁⣀⡡⠀⠀⠴⠒⠗⠋⠉⠉⡆⠀⠆⠄⠄⠘⠀⡎⠀⠀⠀⠑⢅⠑⢼⡀\r\n"
+            		+ "⢯⣉⣓⠒⠒⠤⠤⣄⣀⣀⣀⣀⡀⠐⠁⠀⠀⠀⠒⠀⢀⡀⠀⠀⠀⠀⠀⠑⣌⣇\r\n"
+            		+ "⠀⠈⢳⠄⠈⠀⠤⢄⣀⠀⢈⣉⡹⠯⡟⠁⠀⠀⠀⠀⢸⠀⠀⠂⠀⠀⡠⠚⣡⡿\r\n"
+            		+ "⠀⢠⣋⣀⣀⣀⣀⠤⠭⢛⡩⠄⠒⠩⠂⢀⠄⠀⠀⠀⠈⢢⡀⠀⡠⠋⡩⠋⠀⢳\r\n"
+            		+ "⠀⢹⠤⠬⠤⠬⠭⣉⣉⢃⠀⠀⣀⣀⠀⠁⠀⠀⠀⠀⡞⢺⡈⠋⡢⠊⠀⠀⠀⢸\r\n"
+            		+ "⠀⠈⡆⠁⢀⠀⠀⠀⠉⠋⠉⠓⠂⠤⣀⡀⠀⠀⠀⠀⡧⠊⡠⠦⡈⠳⢄⠀⠀⠈\r\n"
+            		+ "⠀⠀⢹⡜⠀⠁⠀⠀⠒⢤⡄⠤⠔⠶⠒⠛⠧⠀⠀⡼⡠⠊⠀⠀⠙⢦⡈⠳⡄⠀\r\n"
+            		+ "⠀⠀⢸⠆⠀⠈⠀⠠⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⡜⢸⠀⠀⠀⠀⠀⠀⠑⢄⠈⢲\r\n"
+            		+ "⠀⠀⢸⢀⠇⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⡄⠊⢠⠃⠀⠀⠀⠀⠀⠀⠀⠈⡢⣸\r\n"
+            		+ "⠀⠀⠈⠳⣤⣄⡀⠀⠀⠀⠈⠉⠉⠁⠒⠁⠀⠠⣏⠀⠀⠀⠀⠀⠀⢀⣔⠾⡿⠃\r\n"
+            		+ "⠀⠀⠀⠀⠀⠉⠙⠛⠒⠤⠤⣤⣄⣀⣀⣀⣔⣢⣀⣉⣂⣀⣀⣠⠴⠿⠛⠋⠀");
+        }
+    } while (op != 2);
+    sc.close();
+}
+}
+
+
+
+
