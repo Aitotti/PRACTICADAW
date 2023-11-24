eee750d896b56538a54cbcb2ca0e3d1c93092dc8
silvia <silvia6rodriguez@gmail.com>
2023-11-24 20:14:17 +0100

    Version Silvia

---
+++ gitsilvia.java
@@ -0,0 +1,14 @@
+package ejerciciosentornos;
+
+import java.util.Scanner;
+
+public class gitsilvia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String frase="";
+		String cadena="";
+		System.out.println(frase + " " + cadena);	
+	}
+
+}
